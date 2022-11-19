26f7a78c6244c337c5f8cbe82f8fe651f1c33976
Tatiana Kvon <kvn-tn@mail.ru>
2022-11-19 21:43:47 +0300

    add Example005 CodeIfElse

---
+++ Example005_CodeIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "tatiana")
+{
+    Console.WriteLine("Ура, это же Tatiana");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}  
